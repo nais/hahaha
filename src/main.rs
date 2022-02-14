0908fb22682d7a4330f861d27221dcbce5b92b31
Kent D <kent.daleng@nav.no>
2022-02-14 14:58:14 +0100

    use json formatted logs

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use crate::{api::Destroyer, events::Recorder, pod::Sidecars, prometheus::*};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt().json().try_init().unwrap();
 
     let actions = actions::generate();
     let client = Client::try_default().await?;
